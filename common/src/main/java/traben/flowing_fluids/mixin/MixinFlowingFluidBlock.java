ca17905508614b1d76c965eb3af4e7a375d3b204
kuroneko2310 <93176041+kuroneko2310@users.noreply.github.com>
2025-11-22 17:09:06 +0900

    Make shouldSpread injections optional for missing targets

---
+++ common/src/main/java/traben/flowing_fluids/mixin/MixinFlowingFluidBlock.java
@@ -0,0 +1,70 @@
+package traben.flowing_fluids.mixin;
+
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BucketPickup;
+#if MC > MC_20_1
+import net.minecraft.world.level.block.FlowingFluidBlock;
+#else
+import net.minecraft.world.level.block.LiquidBlock;
+#endif
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import traben.flowing_fluids.FFFluidUtils;
+import traben.flowing_fluids.FlowingFluids;
+
+
+#if MC > MC_20_1
+@Mixin(FlowingFluidBlock.class)
+#else
+@Mixin(LiquidBlock.class)
+#endif
+public abstract class MixinFlowingFluidBlock extends Block implements BucketPickup {
+
+    @Shadow
+    @Final
+    protected FlowingFluid fluid;
+
+    public MixinFlowingFluidBlock() {
+        //noinspection DataFlowIssue
+        super(null);
+    }
+
+
+    @ModifyExpressionValue(method = "shouldSpreadLiquid", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/material/Fluid;is(Lnet/minecraft/tags/TagKey;)Z"), require = 0)
+    // this is a real dodgey mixin target but any other way failed to grab the locals
+    private boolean ff$consumeLevelObsidianOrCobbleCreation(final boolean original,
+                                                            @Local(argsOnly = true) Level level,
+                                                            @Local(ordinal = 1) BlockPos blockPos) {
+        if (original && FlowingFluids.config.enableMod && FlowingFluids.config.isFluidAllowed(this.fluid)) {
+            var state = level.getFluidState(blockPos);
+            FFFluidUtils.setFluidStateAtPosToNewAmount(level, blockPos, state.getType(), state.getAmount() - 1);
+        }
+        return original;
+    }
+
+    @WrapOperation(method = "shouldSpreadLiquid", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/material/FluidState;isSource()Z"), require = 0)
+    private boolean ff$modifyObsidianCondition(final FluidState instance, final Operation<Boolean> original) {
+        boolean source = original.call(instance); // so any other mixin may run
+        if (!source
+                && FlowingFluids.config.enableMod
+                && FlowingFluids.config.isFluidAllowed(this.fluid)
+                && instance.getAmount() >= FlowingFluids.config.minLavaLevelForObsidian) {
+            return true;
+        }
+        return source;
+    }
+
+}
--- common/src/main/java/traben/flowing_fluids/mixin/MixinLiquidBlock.java
+++ common/src/main/java/traben/flowing_fluids/mixin/MixinLiquidBlock.java
@@ -1,9 +1,6 @@
 package traben.flowing_fluids.mixin;
 
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 #if MC > MC_20_1
@@ -11,13 +8,11 @@ import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.DataComponents;
 #endif
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BucketPickup;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -40,34 +35,6 @@ public abstract class MixinLiquidBlock extends Block implements BucketPickup {
         super(null);
     }
 
-
-
-
-    @ModifyExpressionValue(method = "shouldSpreadLiquid", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/world/level/material/Fluid;is(Lnet/minecraft/tags/TagKey;)Z"))
-    // this is a real dodgey mixin target but any other way failed to grab the locals
-    private boolean ff$consumeLevelObsidianOrCobbleCreation(final boolean original,
-                                                            @Local(argsOnly = true) Level level,
-                                                            @Local(ordinal = 1) BlockPos blockPos) {
-        if (original && FlowingFluids.config.enableMod && FlowingFluids.config.isFluidAllowed(this.fluid)) {
-            var state = level.getFluidState(blockPos);
-            FFFluidUtils.setFluidStateAtPosToNewAmount(level, blockPos, state.getType(), state.getAmount() - 1);
-        }
-        return original;
-    }
-
-    @WrapOperation(method = "shouldSpreadLiquid", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/world/level/material/FluidState;isSource()Z"))
-    private boolean ff$modifyObsidianCondition(final FluidState instance, final Operation<Boolean> original) {
-        boolean source = original.call(instance); // so any other mixin may run
-        if (!source
-                && FlowingFluids.config.enableMod
-                && FlowingFluids.config.isFluidAllowed(this.fluid)
-                && instance.getAmount() >= FlowingFluids.config.minLavaLevelForObsidian) {
-            return true;
-        }
-        return source;
-    }
 
 
     @Inject(method = "pickupBlock", at = @At(value = "RETURN"), cancellable = true)
